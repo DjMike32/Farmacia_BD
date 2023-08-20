30395023240de2009438d8f3de4da0312faebc3f
DjMike32 <michaelchang277@gmail.com>
2023-08-20 01:30:39 -0600

    Estructura final

--- Backend/app/routes/Routes.js
+++ Backend/app/routes/Routes.js
@@ -1,5 +1,4 @@
 
-
 const express = require('express');
 const router = express.Router();
 const oracledb = require('oracledb');
@@ -16,9 +15,7 @@ app.use('/', router);
 app.use(express.static("Frontend"));
 app.use(express.json());
 
-
 module.exports = router;
-
 
 const dbConfig = {
   user: 'Michael',
@@ -28,118 +25,176 @@ const dbConfig = {
 
 
 
-
-
-router.get('/consultar-Empleados', async (req, res) => {
+router.post('/login', async (req, res) => {
   try {
-    console.log('Intentando conectar a Oracle...');
-    const connection = await oracledb.getConnection();
-    console.log('Conexión a Oracle exitosa');
-    
-    const result = await connection.execute('SELECT * FROM Empleados');
+    const { usuario, password } = req.body;
+
+    // Realizar una consulta a la base de datos para verificar las credenciales
+    const connection = await oracledb.getConnection(dbConfig);
+    const result = await connection.execute(
+      'SELECT COUNT(*) AS count FROM Empleados WHERE Usuario = :usuario AND Password = :password',
+      [usuario, password]
+    );
     connection.close();
 
-    res.json(result.rows);
-  } catch (error) {
-    console.error('Error al realizar la consulta:', error);
-    res.status(500).json({ error: 'Error al realizar la consulta' });
-  }
-});
-
-
-
-router.post('/consultar-Usuario/:usuario', async (req, res) => {
-  try {
-    // dbConfig.user
-    // dbConfig.password
-    console.log('Intentando conectar a Oracle...');
-    const connection = await oracledb.getConnection();
-    console.log('Conexión a Oracle exitosa');
-    
-    const result = await connection.execute('SELECT * FROM Empleados');
-    connection.close();
-    console.log(req.body);
-    res.json(result.rows);
-  } catch (error) {
-    console.error('Error al realizar la consulta:', error);
-    res.status(500).json({ error: 'Error al realizar la consulta' });
-  }
-});
-
-
-
-
-
-
-app.post('/cliente/crearCliente', async (req, res) => {
-  try {
-    const {
-      // ID_Empleado,
-      // Usuario,
-      // Password,
-      // Salario
-      Descripcion,
-      Color
-    } = req.body;
-    console.log(req.body);
-    const connection = await oracledb.getConnection(dbConfig);
-
-    const result = await connection.execute(
-      `
-      INSERT INTO Pais (Descripcion, Color) VALUES (:Descripcion, :Color)
-      `,
-      {
-        // ID_Empleado,
-        // Usuario,
-        // Password,
-        // Salario
-        Descripcion,
-        Color
-      },
-      {
-        autoCommit: true
-      }
-    );
-
-    await connection.close();
-
-    res.json({ message: 'Cliente creado exitosamente.' });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: 'Error al crear el cliente.' });
-  }
-});
-
-
-app.delete('/cliente/eliminarCliente/:ID_Empleado', async (req, res) => {
-  try {
-    const { ID_Empleado } = req.params;
-    const connection = await oracledb.getConnection(dbConfig);
-
-    const result = await connection.execute(
-      `
-      DELETE FROM Empleados WHERE ID_Empleado = :ID_Empleado
-      `,
-      {
-        ID_Empleado
-      },
-      {
-        autoCommit: true
-      }
-    );
-
-    await connection.close();
-
-    if (result.rowsAffected === 1) {
-      res.json({ message: 'Cliente eliminado exitosamente.' });
+    const count = result.rows[0].COUNT;
+    if (count > 0) {
+      res.json({ success: true });
     } else {
-      res.status(404).json({ message: 'Cliente no encontrado.' });
+      res.json({ success: false });
     }
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: 'Error al eliminar el cliente.' });
+  } catch (error) {
+    console.error('Error en inicio de sesión:', error);
+    res.status(500).json({ success: false });
   }
 });
+
+
+router.get('/medicamento', async (req, res) => {
+  try {
+      // Realizar la consulta a la base de datos para obtener todos los valores de Medicamentos
+      const connection = await oracledb.getConnection(dbConfig);
+      const result = await connection.execute(
+          'SELECT * FROM Medicamento'
+      );
+      connection.close();
+
+      // Enviar los resultados como respuesta JSON
+      res.json(result.rows);
+  } catch (error) {
+      console.error('Error al obtener medicamentos:', error);
+      res.status(500).json({ success: false, message: 'Error al obtener medicamentos' });
+  }
+});
+
+
+
+
+
+
+
+
+module.exports = router;
+
+
+app.listen(3000, () => console.log('Server started on port 3000'));
+
+
+
+
+
+
+
+
+
+
+// // Ruta para consultar empleados
+// router.get('/consultar-Empleados', async (req, res) => {
+//   try {
+//     console.log('Intentando conectar a Oracle...');
+//     const connection = await oracledb.getConnection();
+//     console.log('Conexión a Oracle exitosa');
+    
+//     const result = await connection.execute('SELECT * FROM Empleados');
+//     connection.close();
+
+//     res.json(result.rows);
+//   } catch (error) {
+//     console.error('Error al realizar la consulta:', error);
+//     res.status(500).json({ error: 'Error al realizar la consulta' });
+//   }
+// });
+
+
+// router.get('/consultar-Usuario/:usuario', async (req, res) => {
+//   try {
+//     console.log('Intentando conectar a Oracle...');
+//     const connection = await oracledb.getConnection();
+//     console.log('Conexión a Oracle exitosa');
+//     const result = await connection.execute('SELECT * FROM Pais WHERE ID_Pais = :usuario');
+//     connection.close();
+//     console.log(req.body);
+//     res.json(result.rows);
+//   } catch (error) {
+//     console.error('Error al realizar la consulta:', error);
+//     res.status(500).json({ error: 'Error al realizar la consulta' });
+//   }
+// });
+
+
+
+
+
+
+// app.post('/cliente/crearCliente', async (req, res) => {
+//   try {
+//     const {
+//       // ID_Empleado,
+//       // Usuario,
+//       // Password,
+//       // Salario
+//       Descripcion,
+//       Color
+//     } = req.body;
+//     console.log(req.body);
+//     const connection = await oracledb.getConnection(dbConfig);
+
+//     const result = await connection.execute(
+//       `
+//       INSERT INTO Pais (Descripcion, Color) VALUES (:Descripcion, :Color)
+//       `,
+//       {
+//         // ID_Empleado,
+//         // Usuario,
+//         // Password,
+//         // Salario
+//         Descripcion,
+//         Color
+//       },
+//       {
+//         autoCommit: true
+//       }
+//     );
+
+//     await connection.close();
+
+//     res.json({ message: 'Cliente creado exitosamente.' });
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).json({ message: 'Error al crear el cliente.' });
+//   }
+// });
+
+
+// app.delete('/cliente/eliminarCliente/:ID_Empleado', async (req, res) => {
+//   try {
+//     const { ID_Empleado } = req.params;
+//     const connection = await oracledb.getConnection(dbConfig);
+
+//     const result = await connection.execute(
+//       `
+//       DELETE FROM Empleados WHERE ID_Empleado = :ID_Empleado
+//       `,
+//       {
+//         ID_Empleado
+//       },
+//       {
+//         autoCommit: true
+//       }
+//     );
+
+//     await connection.close();
+
+//     if (result.rowsAffected === 1) {
+//       res.json({ message: 'Cliente eliminado exitosamente.' });
+//     } else {
+//       res.status(404).json({ message: 'Cliente no encontrado.' });
+//     }
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).json({ message: 'Error al eliminar el cliente.' });
+//   }
+// });
 
 
 
@@ -167,51 +222,6 @@ app.delete('/cliente/eliminarCliente/:ID_Empleado', async (req, res) => {
 //     res.status(500).json({ success: false });
 //   }
 // });
-
-router.post('/login', async (req, res) => {
-  try {
-    const { Descripcion, Color } = req.body;
-
-    // Realizar una consulta a la base de datos para verificar las credenciales
-    const connection = await oracledb.getConnection(dbConfig);
-    const result = await connection.execute(
-      'SELECT * FROM Pais WHERE Descripcion = :Descripcion AND Color = :Color',
-      [Descripcion, Color]
-    );
-    connection.close();
-
-    if (result.rows.length > 0) {
-      res.json({ success: true });
-    } else {
-      res.json({ success: false });
-    }
-  } catch (error) {
-    console.error('Error en inicio de sesión:', error);
-    res.status(500).json({ success: false });
-  }
-});
-
-
-
-
-
-
-
-
-module.exports = router;
-
-app.listen(3000, () => console.log('Server started on port 3000'));
-
-
-
-
-
-
-
-
-
-
-
 
 
 
--- Frontend/JS/index.js
+++ Frontend/JS/index.js
@@ -1,24 +1,4 @@
 
-// let Usuarios = {}
-// let Medicamentos ={}
-
-// const cargarUsuarios= async () => {
-//     const respuesta = await fetch('/consultar-Empleados', {
-//         method: "get"});
-//         Usuarios = await respuesta.json();
-//         console.log(Usuarios);
-//     }
-
-// const cargarMedicamentos= async () => {
-//     const respuesta = await fetch('/consultar-Medicamentos', {
-//         method: "get"});
-//         Medicamentos = await respuesta.json();
-//         console.log(Medicamentos);
-//     }    
-    
-// cargarUsuarios();
-// cargarMedicamentos();
-
 const cargarUsuarios = async () => {
   try {
     const respuesta = await fetch('/consultar-Empleados');
@@ -29,25 +9,6 @@ const cargarUsuarios = async () => {
   }
 };
 
-
-
-
-const IniciarSesion = async () => {
-  try {
-    /** Obteniene datos para ingresas */
-    let user = document.getElementById('username').value;
-    let password = document.getElementById('password').value;
-    
-    const respuesta = await fetch('/consultar-Usuario/:usuario');
-    const usuarios = await respuesta.json();
-    console.log( user, password);
-  } catch (error) {
-    console.error('Error al cargar usuarios:', error);
-  }
-};
-
-
-//cargarUsuarios();
 
 
 // Llamado a fetch para crear un empleado
@@ -176,19 +137,63 @@ const crearEmpleado = async (Usuario, Password, Salario) => {
 //     });
 // });
 
+// document.getElementById('loginForm').addEventListener('submit', function(event) {
+//   event.preventDefault();
+
+//   const Usuario = document.getElementById('usuario').value;
+//   const Password = document.getElementById('password').value;
+
+//   console.log('Usuario:', Usuario);
+//   console.log('Password:', Password);
+
+//   const credenciales = {
+//     Usuario: Usuario,
+//     Password: Password 
+//   };
+
+//   fetch('/login', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify(credenciales)
+//   })
+//     .then(response => response.json())
+//     .then(data => {
+//       if (data.success) {
+//         alert('Inicio de sesión exitoso');
+//       } else {
+//         alert('Credenciales incorrectas');
+//       }
+//     })
+//     .catch(error => {
+//       console.error('Error:', error);
+//       alert('Error al iniciar sesión');
+//     });
+// });
+
+function redireccionar() {
+  // Redirige al usuario a la página de destino
+  window.location.href = "admiMenu.html";
+  setTimeout(function() {
+    window.location.href = "pagina_destino.html";
+}, 2000);
+}
+
+
 document.getElementById('loginForm').addEventListener('submit', function(event) {
   event.preventDefault();
 
-  const Descripcion = document.getElementById('usuario').value;
-  const Color = document.getElementById('password').value;
-
-  console.log('Descripcion:', Descripcion);
-  console.log('Color:', Color);
+  const usuario = document.getElementById('usuario').value;
+  const password = document.getElementById('password').value;
 
   const credenciales = {
-    Descripcion: Descripcion,
-    Color: Color 
+    usuario: usuario,
+    password: password
   };
+
+    console.log('Usuario:', usuario);
+  console.log('Password:', password);
 
   fetch('/login', {
     method: 'POST',
@@ -206,10 +211,53 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
       }
     })
     .catch(error => {
-      console.error('Error:', error);
-      alert('Error al iniciar sesión');
+      // console.error('Error:', error);
+      // alert('Error al iniciar sesión');
     });
+    redireccionar();
 });
+
+
+    // Arreglo para almacenar los datos de medicamentos
+    var medicamentos = [];
+
+    // Función para obtener los datos de la base de datos
+    async function obtenerDatosMedicamentos() {
+        try {
+            const response = await fetch('/medicamento'); // Reemplaza con la ruta correcta
+            const data = await response.json();
+            medicamentos = data;
+            generarFilasTabla();
+        } catch (error) {
+            console.error('Error al obtener los datos:', error);
+        }
+    }
+
+    // Llamada a la función para obtener los datos
+    obtenerDatosMedicamentos();
+
+    // Función para generar las filas de la tabla con los datos obtenidos
+    // function generarFilasTabla() {
+    //     var tabla = document.getElementById("tabla-medicamentos");
+
+    //     medicamentos.forEach(function(medicamento) {
+    //         var fila = tabla.insertRow();
+
+    //         fila.insertCell().textContent = medicamento.ID_Medicamentos;
+    //         fila.insertCell().textContent = medicamento.Descripcion;
+    //         fila.insertCell().textContent = medicamento.Nombre_Medicamento;
+    //         fila.insertCell().textContent = medicamento.Fecha_Vencimiento;
+    //         fila.insertCell().textContent = medicamento.Fecha_Laboracion;
+    //         fila.insertCell().textContent = medicamento.Prohibiciones;
+    //         fila.insertCell().textContent = medicamento.Existencias;
+    //         fila.insertCell().textContent = medicamento.ID_Drogueria;
+    //         fila.insertCell().textContent = medicamento.Nombre_Medicamento;
+    //         fila.insertCell().textContent = medicamento.No_Lote;
+    //         fila.insertCell().textContent = medicamento.ID_Tipo_Medicamento;
+    //     });
+    // }
+
+
 
 
 
